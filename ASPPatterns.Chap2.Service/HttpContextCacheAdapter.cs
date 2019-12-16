2baa13fd4df0de2d7cfba38f4911bf01fdaab837
Callum Johnson <callumjohnson57@yahoo.com>
2019-12-16 08:25:18 +0000

    Initial commit

---
+++ ASPPatterns.Chap2.Service/HttpContextCacheAdapter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ASPPatterns.Chap2.Service
+{
+	namespace ASPPatterns.Chap2.Service
+	{
+		public class HttpContextCacheAdapter : ICacheStorage
+		{
+			private IHttpContextAccessor _contextAccessor;
+
+			public HttpContextCacheAdapter(IHttpContextAccessor contextAccessor)
+			{
+				_contextAccessor = contextAccessor;
+			}
+
+			public void Remove(string key)
+			{
+				_contextAccessor.HttpContext.Items[key] = null;
+			}
+
+			public void Store(string key, object data)
+			{
+				_contextAccessor.HttpContext.Items[key] = data;
+			}
+
+			public T Retrieve<T>(string key)
+			{
+				T itemStored = (T)_contextAccessor.HttpContext.Items[key];
+
+				if (itemStored == null)
+					itemStored = default(T);
+
+				return itemStored;
+			}
+		}
+	}
+}
---
+++ ASPPatterns.Chap2.Service/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap2.Service
+{
+    public class Product
+    {
+		// Hold the attributes of the product.
+    }
+}
